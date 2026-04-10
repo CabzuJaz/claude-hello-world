0763e054ca66063256f37e1fb9a0f3779caaab56
Jazzmin Sicat-Cabizares <jazzmincabizares@gmail.com>
2026-04-10 09:06:08 -0500

    feat: Day 3-Tool-Used

---
+++ day03-tool-use/tool_use.py
@@ -0,0 +1,27 @@
+import anthropic
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+# --- Step 1: Define your tools (what Claude can call) ---
+tools = [
+    {
+        "name": "get_weather",
+        "description": "Get the current weather for a city",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+                "city": {
+                    "type": "string",
+                    "description": "The city name, e.g Manila"
+                }
+            },
+            "required": ["city"]
+        }
+}
+
+]
